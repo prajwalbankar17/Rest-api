1df74b98602a73c74e712bba174b78bc1b1539c6
prajwalbankar17 <prajwalbankar11@gmail.com>
2025-07-31 17:01:08 +0530

    Post Delete API

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,45 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  }
-];
+// const users = [
+//   {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 25,
+//   },
+//   {
+//     firstName: "Jane",
+//     lastName: "Doe",
+//     age: 24,
+//   },
+// ];
+
+let users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
 });
 
 router.post("/", (req, res) => {
-    console.log('POST ROUTE REACHED');
-    res.send('POST ROUTE REACHED');
+  const user = req.body;
+  users.push({ ...user, id: uuidv4()});
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with the id ${id} deleted from the database.`);
 });
 
 export default router;
-
