63f772a009c66539bd9d65ed6fb8b9dca90b63a2
prajwalbankar17 <prajwalbankar11@gmail.com>
2025-07-31 18:03:55 +0530

    Done with get, put, patch, delete, update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
-
-
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
 const router = express.Router();
 
 // const users = [
@@ -17,29 +21,15 @@ const router = express.Router();
 //   },
 // ];
 
-let users = [];
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/", getUsers);
 
-router.post("/", (req, res) => {
-  const user = req.body;
-  users.push({ ...user, id: uuidv4()});
+router.post("/", createUser);
 
-  res.send(`User with the name ${user.firstName} added to the database!`);
-});
+router.delete("/:id", deleteUser);
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const foundUser = users.find((user) => user.id === id);
-  res.send(foundUser);
-});
+router.get("/:id", getUser);
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-  res.send(`User with the id ${id} deleted from the database.`);
-});
+router.patch("/:id", updateUser);
 
 export default router;
